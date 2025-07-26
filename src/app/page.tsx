50b22938d2d5d16fa3cd74ee0a32e87e248dbc60
jeffreyai7 <jirukeh@gmail.com>
2025-07-26 18:03:17 +0100

    finish stats component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Header from "@/components/Navbar";
+import StatsSection from "@/components/StatsSection";
 
 export default function App() {
   return (
     <main className="w-full md:w-[80%] mx-auto">
       <Header />
       <Hero />
+      <StatsSection />
     </main>
   );
 }
