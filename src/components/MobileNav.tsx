bc058a8e2adb9dfc8e48490eaf3eb9deae148185
Jeffreyai7 <jirukeh@gmail.com>
2026-01-08 11:06:01 +0100

    updated the padding

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -17,7 +17,7 @@ const MobileNav = ({
   return (
     <div
       className={cn(
-        "absolute top-20 left-0 z-50 flex w-screen flex-col items-center justify-between gap-14 bg-primary-foreground/95 backdrop-blur-md py-10 shadow-2xl transition duration-500 lg:relative lg:top-0 lg:w-full lg:flex-row lg:gap-0 lg:py-0 lg:shadow-none",
+        "absolute top-16 left-0 z-50 flex w-screen flex-col items-center justify-between gap-14 bg-primary-foreground/95 backdrop-blur-md py-10 shadow-2xl transition duration-500 lg:relative lg:top-0 lg:w-full lg:flex-row lg:gap-0 lg:py-0 lg:shadow-none",
         showNav ? "translate-y-0" : "-translate-y-[200%] lg:translate-y-0"
       )}
     >
